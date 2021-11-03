0774d75d607bb05ed38cd077b73c41602abc4653
waketakuo <takuowake@gmail.com>
2021-11-03 11:23:35 +0900

    first_commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module singlepage
+
+go 1.16
+
+require (
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
+	github.com/klebervirgilio/vue-crud-app-with-golang v0.0.0-20181007122050-8c8603909d12 // indirect
+)
---
+++ pkg/kudo/service.go
@@ -0,0 +1,63 @@
+package kudo
+
+import (
+	"singlepage/pkg/core"
+	"strconv"
+)
+
+type CitHubRepo struct {
+	RepoID int64 `json:"id"`
+	RepoURL string `json:"html_url"`
+	RepoName string `json:"full_name"`
+	Language string `json:"language"`
+	Description string `json:"description"`
+	Notes string `json:"notes"`
+}
+
+type Service struct {
+	userId string
+	repo core.Repository
+}
+
+func (s Service) GetKudos()([]*core.Kudo, error) {
+	return s.repo.FindAll(map[string]interface{}{"userId":s.userId})
+}
+
+func (s Service) CreateKudoFor(githubRepo GitHubRepo)(*core.Kudo, error) {
+	kudo := s.githubRepoToKudo(githubRepo)
+	err := s.repo.Create(kudo)
+	if err != nil {
+		return nil, err
+	}
+	return kudo, nil
+}
+
+func (s Service) UpdateKudoWith(githubRepo GitHubRepo)(*core.Kudo, error) {
+	kudo := s.githubRepoToKudo(githubRepo)
+	err := s.repo.Create(kudo)
+	if err != nil {
+		return nil, err
+	}
+	return kudo, nil
+}
+
+func (s Servive) RemoveKudo(githubRepo GitHubRepo)(*core.Kudo, error) {
+	kudo := s.githubRepoToKudo(githubRepo)
+	err := s.repo.Delete(kudo)
+	if err != nil {
+		return nil, err
+	}
+	return kudo, nil
+}
+
+func (s Service) githubRepoToKudo(githubRepo GitHubRepo) *core.Kudo {
+	return &core.Kudo {
+		UserID: s.userId,
+		RepoID: strconv.Itoa(int(githubRepo.RepoID)),
+		RepoName: githubRepo.RepoName,
+		RepoURL: githubRepo.RepoURL,
+		Language: githubRepo.Language,
+		Description: githubRepo.Description,
+		Notes: githubRepo.Notes,
+	}
+}
